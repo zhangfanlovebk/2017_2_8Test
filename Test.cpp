c5e2f210f82577f8e2f0f02d8cc6edceb05dfa07
Your Name <you@example.com>
2017-03-04 11:51:28 +0800

    定义对象测试

--- Test.cpp
+++ Test.cpp
@@ -423,332 +423,376 @@
 
 
 //写出String类浅拷贝的形式，并分析存在的问题
-#include<iostream>
-#include<malloc.h>
+//#include<iostream>
+//#include<malloc.h>
+//
+//using namespace std;
+////实现String 类
+////浅拷贝存在的问题：
+//
+////1.浅拷贝只进行值的拷贝，对于指针类型的数据成员来说，会指向同一块分配的堆区，释放的时候会把一块空间释放多次
+////2.浅拷贝的赋值函数来说，值拷贝的形式会泄露掉当前对象之前所指的空间。
+////3浅拷贝的拷贝构造函数不开辟新的空间，对于一个指针来说，会写入失败，造成严重的后果。
+//
+////浅拷贝
+//class String
+//{
+//	String(const char*pStr = "")
+//	{
+//		if (pStr != NULL)
+//		{
+//			_pStr = new char[strlen(pStr) + 1];
+//			strcpy_s(_pStr, strlen(pStr) + 1,pStr);
+//		}
+//		else
+//		{
+//			_pStr = new char[ 1];
+//			*_pStr = '\0';
+//		}
+//	}
+//
+//	//拷贝构造函数
+//	String(const String&s)
+//		:_pStr(s._pStr)
+//	{
+//	
+//	}
+//
+//	//赋值函数
+//	String& operator=(const String&s)
+//	{
+//		if (this != &s)
+//		{
+//			_pStr = s._pStr;
+//		}
+//		return *this;
+//	}
+//
+//
+//	~String()
+//	{
+//		if (_pStr != NULL)
+//		{
+//			delete[]_pStr;
+//			_pStr = NULL;
+//		}
+//	}
+//
+//private:
+//	char* _pStr;
+//};
+//
+//
+////写出简洁版本深拷贝的String类
+//class String
+//{
+//	String(const char*pStr = "")
+//	{
+//		if (pStr != NULL)
+//		{
+//			_pStr = new char[strlen(pStr) + 1];
+//			strcpy_s(_pStr, strlen(pStr) + 1, pStr);
+//		}
+//		else
+//		{
+//			_pStr = new char[1];
+//			*_pStr = '\0';
+//		}
+//	}
+//
+//	//拷贝构造函数
+//	String(const String&s)
+//	{
+//	
+//		String temp(s._pStr);
+//		//走完初始化列表只是空间给出来了，_pStr为随机值-》一旦交换进行释放，可能会崩溃
+//		_pStr = NULL;
+//		std::swap(_pStr,temp._pStr);	
+//	}
+//
+//	//赋值函数
+//	//方法
+//	String& operator=(const String&s)
+//	{
+//		if (this != &s)
+//		{
+//			String temp(s);
+//			delete[]_pStr;
+//			_pStr = NULL;
+//			std::swap(_pStr, temp._pStr);
+//		}
+//		return *this;
+//	}
+//
+//	//方法二
+//	//String& operator=( String s)
+//	//{
+//	//	std::swap(_pStr, s._pStr);
+//	//	return *this;
+//	//
+//	//}
+//
+//	~String()
+//	{
+//		if (_pStr != NULL)
+//		{
+//			delete[]_pStr;
+//			_pStr = NULL;
+//		}
+//	}
+//
+//private:
+//	char* _pStr;
+//};
+//
+//
+////写出使用指针作为引用计数处理的String类，并分析存在问题
+////使用指针解决的浅拷贝
+//class String
+//{
+//public:
+//	String(const char*pStr = "")
+//	:_pCount(new int(1))
+//	{
+//		if (pStr != NULL)
+//		{
+//			_pStr = new char[strlen(pStr) + 1];
+//			strcpy_s(_pStr, strlen(pStr) + 1, pStr);
+//		}
+//		else
+//		{
+//			_pStr = new char[1];
+//			*_pStr = '\0';
+//		}
+//
+//	}
+//	//拷贝构造函数
+//	String(const String&s)
+//		:_pStr(s._pStr)
+//		,_pCount(s._pCount)
+//	{
+//
+//		++(*_pCount);
+//		//_pStr = s._pStr;
+//	}
+//
+//	//赋值函数->释放原有空间->拷贝考虑自赋值
+//	String& operator =(const String& s)
+//	{
+//		if (_pStr != s._pStr)
+//		{
+//			//要检查是否是自赋值，因为调用赋值函数，就说明是两个对象，（可能成员函数指向了同一块空间），若不判断则有两种肯能，1.pCout++2.销毁对象都不对
+//			if ((0 == --(*_pCount)) && (_pStr != NULL))
+//			{
+//				delete[] _pStr;
+//				_pStr = NULL;
+//				delete _pCount;
+//				_pCount = NULL;
+//
+//			}
+//			else
+//			{
+//				--(*_pCount);
+//			}
+//				++(*s._pCount);
+//				_pStr = s._pStr;
+//				_pCount = s._pCount;
+//		}
+//		return *this;
+//	}
+//
+//
+//	//析构函数
+//	~String()
+//	{
+//		if ((0 == --*_pCount) && (_pStr != NULL))
+//		{
+//			delete[]_pStr;
+//			_pStr = NULL;
+//
+//			delete _pCount;
+//			_pCount = NULL;
+//		}
+//	
+//	}
+//
+//private:
+//
+//	char* _pStr;
+//	int * _pCount;
+//
+//};
+////->容易出错->写时拷贝
+//
+//
+////写时拷贝的String类
+//class String
+//{
+//public:
+//	//构造函数
+//	String(const char*pStr="")
+//	{
+//		if (pStr == NULL)
+//		{
+//			_pStr = new char[1+4];
+//			*(int*)_pStr = 1;
+//			_pStr += 1;
+//			*(char*)_pStr = '\0';//此处要不要强转？
+//		}
+//		else
+//		{
+//			_pStr = new char[strlen(pStr) + 1 + 4];
+//			*(int*)_pStr = 1;
+//			_pStr += 1;
+//			strcpy_s((char*)_pStr, strlen(pStr) + 1 , pStr);
+//
+//		}
+//	
+//	}
+//
+//	//计数引用的获取
+//	int& GetRef()
+//	{
+//		return *(((int*)_pStr) - 1);
+//	}
+//
+//	//释放该对象所有空间
+//	void Realease()
+//	{
+//		if ((0 == --GetRef())&&(_pStr!=NULL))
+//		{
+//			_pStr -= 4;
+//			delete[]_pStr;
+//			_pStr = NULL;
+//		}
+//	}
+//
+//
+////拷贝构造函数->
+//	String(String&s)
+//	{
+//		++s.GetRef();
+//		_pStr = s._pStr;
+//	
+//	}
+//
+//	//最标准赋值函数->释放原有空间-》开辟新空间-》拷贝
+//	String& operator=( String&s)
+//	{
+//		//if ((0 == --GetRef()) && (_pStr != NULL))
+//		//{
+//		//	delete[]_pStr;//开辟了多少空间，位置要进行更正
+//		//}
+//		
+//		//判断是否是自赋值
+//		if (_pStr != s._pStr)
+//		{
+//			Realease();//释放空间
+//			_pStr = s._pStr;
+//			++s.GetRef();
+//
+//		}
+//		return *this;
+//	}
+//
+//	//完成s[3]='\0';->重载operator[]
+//	char& operator[](size_t size)
+//	{
+//		if (GetRef()> 1)
+//		{
+//			--GetRef();
+//			char* pTemp = new char[strlen(_pStr) + 1 + 4];
+//			*(int*)pTemp = 1;
+//			pTemp += 1;
+//			strcpy_s((char*)pTemp, strlen(_pStr) + 1, _pStr);
+//			//strcpy_s((char*)_pStr,strle)
+//			_pStr = pTemp;
+//
+//		}
+//		return *(_pStr + size);
+//	}
+//	
+//	const char& operator[](size_t size)const
+//	{
+//		return *(_pStr + size);
+//	}
+//	//一个对象改变其余都进行了改变，怎么解决。
+//
+//private:
+//	char *_pStr;
+//};
+//
+//void FunTest0()
+//{
+//	String s1("hello,world");
+//	String s2("1111");
+//	//s2 = s1;
+//	//s2[3] = '9';
+//	char c = s2[5];
+//
+//	String s4(s2);
+//	String s5;
+//	int a = 0;
+//}
+//
+//void FunTest1()//测试一个对象对其余对象的影响
+//{
+//	String s1("hello");
+//	String s2(s1);
+//	String s3(s2);
+//	s3[2] = 'e';
+//	int a = 10;
+//}
+//
+//int main()
+//{
+//	//FunTest0();
+//
+//	FunTest1();
+//	system("pause");
+//	return 0;
+//}
 
+#include <iostream>
 using namespace std;
-//实现String 类
-//浅拷贝存在的问题：
 
-//1.浅拷贝只进行值的拷贝，对于指针类型的数据成员来说，会指向同一块分配的堆区，释放的时候会把一块空间释放多次
-//2.浅拷贝的赋值函数来说，值拷贝的形式会泄露掉当前对象之前所指的空间。
-//3浅拷贝的拷贝构造函数不开辟新的空间，对于一个指针来说，会写入失败，造成严重的后果。
-
-//浅拷贝
-class String
-{
-	String(const char*pStr = "")
-	{
-		if (pStr != NULL)
-		{
-			_pStr = new char[strlen(pStr) + 1];
-			strcpy_s(_pStr, strlen(pStr) + 1,pStr);
-		}
-		else
-		{
-			_pStr = new char[ 1];
-			*_pStr = '\0';
-		}
-	}
-
-	//拷贝构造函数
-	String(const String&s)
-		:_pStr(s._pStr)
-	{
-	
-	}
-
-	//赋值函数
-	String& operator=(const String&s)
-	{
-		if (this != &s)
-		{
-			_pStr = s._pStr;
-		}
-		return *this;
-	}
-
-
-	~String()
-	{
-		if (_pStr != NULL)
-		{
-			delete[]_pStr;
-			_pStr = NULL;
-		}
-	}
-
-private:
-	char* _pStr;
-};
-
-
-//写出简洁版本深拷贝的String类
-class String
-{
-	String(const char*pStr = "")
-	{
-		if (pStr != NULL)
-		{
-			_pStr = new char[strlen(pStr) + 1];
-			strcpy_s(_pStr, strlen(pStr) + 1, pStr);
-		}
-		else
-		{
-			_pStr = new char[1];
-			*_pStr = '\0';
-		}
-	}
-
-	//拷贝构造函数
-	String(const String&s)
-	{
-	
-		String temp(s._pStr);
-		//走完初始化列表只是空间给出来了，_pStr为随机值-》一旦交换进行释放，可能会崩溃
-		_pStr = NULL;
-		std::swap(_pStr,temp._pStr);	
-	}
-
-	//赋值函数
-	//方法
-	String& operator=(const String&s)
-	{
-		if (this != &s)
-		{
-			String temp(s);
-			delete[]_pStr;
-			_pStr = NULL;
-			std::swap(_pStr, temp._pStr);
-		}
-		return *this;
-	}
-
-	//方法二
-	//String& operator=( String s)
-	//{
-	//	std::swap(_pStr, s._pStr);
-	//	return *this;
-	//
-	//}
-
-	~String()
-	{
-		if (_pStr != NULL)
-		{
-			delete[]_pStr;
-			_pStr = NULL;
-		}
-	}
-
-private:
-	char* _pStr;
-};
-
-
-//写出使用指针作为引用计数处理的String类，并分析存在问题
-//使用指针解决的浅拷贝
-class String
+class TPoint
 {
 public:
-	String(const char*pStr = "")
-	:_pCount(new int(1))
+	void SetPoint(int x,int y);
+	int Xcoord()
 	{
-		if (pStr != NULL)
-		{
-			_pStr = new char[strlen(pStr) + 1];
-			strcpy_s(_pStr, strlen(pStr) + 1, pStr);
-		}
-		else
-		{
-			_pStr = new char[1];
-			*_pStr = '\0';
-		}
-
+		return X;
 	}
-	//拷贝构造函数
-	String(const String&s)
-		:_pStr(s._pStr)
-		,_pCount(s._pCount)
+	int Ycoord()
 	{
-
-		++(*_pCount);
-		//_pStr = s._pStr;
+		return Y;
 	}
-
-	//赋值函数->释放原有空间->拷贝考虑自赋值
-	String& operator =(const String& s)
-	{
-		if (_pStr != s._pStr)
-		{
-			//要检查是否是自赋值，因为调用赋值函数，就说明是两个对象，（可能成员函数指向了同一块空间），若不判断则有两种肯能，1.pCout++2.销毁对象都不对
-			if ((0 == --(*_pCount)) && (_pStr != NULL))
-			{
-				delete[] _pStr;
-				_pStr = NULL;
-				delete _pCount;
-				_pCount = NULL;
-
-			}
-			else
-			{
-				--(*_pCount);
-			}
-				++(*s._pCount);
-				_pStr = s._pStr;
-				_pCount = s._pCount;
-		}
-		return *this;
-	}
-
-
-	//析构函数
-	~String()
-	{
-		if ((0 == --*_pCount) && (_pStr != NULL))
-		{
-			delete[]_pStr;
-			_pStr = NULL;
-
-			delete _pCount;
-			_pCount = NULL;
-		}
-	
-	}
-
+	void Move(int xOffset,int yOffset);
 private:
-
-	char* _pStr;
-	int * _pCount;
-
-};
-//->容易出错->写时拷贝
-
-
-//写时拷贝的String类
-class String
-{
-public:
-	//构造函数
-	String(const char*pStr="")
-	{
-		if (pStr == NULL)
-		{
-			_pStr = new char[1+4];
-			*(int*)_pStr = 1;
-			_pStr += 1;
-			*(char*)_pStr = '\0';//此处要不要强转？
-		}
-		else
-		{
-			_pStr = new char[strlen(pStr) + 1 + 4];
-			*(int*)_pStr = 1;
-			_pStr += 1;
-			strcpy_s((char*)_pStr, strlen(pStr) + 1 , pStr);
-
-		}
-	
-	}
-
-	//计数引用的获取
-	int& GetRef()
-	{
-		return *(((int*)_pStr) - 1);
-	}
-
-	//释放该对象所有空间
-	void Realease()
-	{
-		if ((0 == --GetRef())&&(_pStr!=NULL))
-		{
-			_pStr -= 4;
-			delete[]_pStr;
-			_pStr = NULL;
-		}
-	}
-
-
-//拷贝构造函数->
-	String(String&s)
-	{
-		++s.GetRef();
-		_pStr = s._pStr;
-	
-	}
-
-	//最标准赋值函数->释放原有空间-》开辟新空间-》拷贝
-	String& operator=( String&s)
-	{
-		//if ((0 == --GetRef()) && (_pStr != NULL))
-		//{
-		//	delete[]_pStr;//开辟了多少空间，位置要进行更正
-		//}
-		
-		//判断是否是自赋值
-		if (_pStr != s._pStr)
-		{
-			Realease();//释放空间
-			_pStr = s._pStr;
-			++s.GetRef();
-
-		}
-		return *this;
-	}
-
-	//完成s[3]='\0';->重载operator[]
-	char& operator[](size_t size)
-	{
-		if (GetRef()> 1)
-		{
-			--GetRef();
-			char* pTemp = new char[strlen(_pStr) + 1 + 4];
-			*(int*)pTemp = 1;
-			pTemp += 1;
-			strcpy_s((char*)pTemp, strlen(_pStr) + 1, _pStr);
-			//strcpy_s((char*)_pStr,strle)
-			_pStr = pTemp;
-
-		}
-		return *(_pStr + size);
-	}
-	
-	const char& operator[](size_t size)const
-	{
-		return *(_pStr + size);
-	}
-	//一个对象改变其余都进行了改变，怎么解决。
-
-private:
-	char *_pStr;
+	int X,Y;
 };
 
-void FunTest0()
+void TPoint::SetPoint(int x,int y)
 {
-	String s1("hello,world");
-	String s2("1111");
-	//s2 = s1;
-	//s2[3] = '9';
-	char c = s2[5];
-
-	String s4(s2);
-	String s5;
-	int a = 0;
+	X = x;
+	Y = y;
 }
 
-void FunTest1()//测试一个对象对其余对象的影响
+void TPoint::Move(int xOffset,int yOffset)
 {
-	String s1("hello");
-	String s2(s1);
-	String s3(s2);
-	s3[2] = 'e';
-	int a = 10;
+	X += xOffset;
+	Y += yOffset;
 }
 
 int main()
 {
-	//FunTest0();
-
-	FunTest1();
-	system("pause");
+	TPoint p1,p2;
+	p1.SetPoint(3,5);
+	p2.SetPoint(8,10);
+	p1.Move(2,1);
+	p2.Move(1,-2);
+	cout<<"x1 = "<<p1.Xcoord()<<",y1 = "<<p1.Ycoord()<<endl;
+	cout<<"x2 = "<<p2.Xcoord()<<",y2 = "<<p2.Ycoord()<<endl;
 	return 0;
 }
